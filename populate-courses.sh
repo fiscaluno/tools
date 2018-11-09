351bbc85f62715012225bef38869f860ce4836fc
Vinicius Luiz <viniciuslcp97@gmail.com>
2018-11-09 01:11:13 -0200

    convert tabs to spaces

--- populate-courses.sh
+++ populate-courses.sh
@@ -6,24 +6,24 @@ PERIODS=("nightly" "morning")
 DEGREES=("Bachelor" "Graduate" "Master's" "Doctoral")
 
 function postCourse {
-	NAME=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
-	PERIOD=${PERIODS[$RANDOM % ${#RANDOM[*]}]}
-	DEGREE=${DEGREES[$RANDOM % ${#RANDOM[*]}]}
+    NAME=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
+    PERIOD=${PERIODS[$RANDOM % ${#RANDOM[*]}]}
+    DEGREE=${DEGREES[$RANDOM % ${#RANDOM[*]}]}
 
-	curl -X POST \
-		https://athena-bff.herokuapp.com/v1/courses \
-	  	-H 'cache-control: no-cache' \
-	  	-H 'content-type: application/json' \
-	  	-H 'postman-token: a0ff56d8-4393-d1a3-7960-fd27fc61edae' \
-	  	-d '{
-			"course_name": "Course '"$NAME"'",
-			"course_type": "'"$DEGREE"'",
-			"course_periods": ["'"$PERIOD"'"],
-			"course_time_to_graduate_range": ['"$(shuf -i 4-10 -n 1)"'],
-			"course_monthly_value_range": ['"$(shuf -i 550-1800 -n 1)"', '"$(shuf -i 550-1800 -n 1)"'],
-			"institution_id": '"$RANDOM"',
-			"institution_name": "Institution '"$NAME"'"
-	   	}'
+    curl -X POST \
+    https://athena-bff.herokuapp.com/v1/courses \
+        -H 'cache-control: no-cache' \
+        -H 'content-type: application/json' \
+        -H 'postman-token: a0ff56d8-4393-d1a3-7960-fd27fc61edae' \
+        -d '{
+            "course_name": "Course '"$NAME"'",
+            "course_type": "'"$DEGREE"'",
+            "course_periods": ["'"$PERIOD"'"],
+            "course_time_to_graduate_range": ['"$(shuf -i 4-10 -n 1)"'],
+            "course_monthly_value_range": ['"$(shuf -i 550-1800 -n 1)"', '"$(shuf -i 550-1800 -n 1)"'],
+            "institution_id": '"$RANDOM"',
+            "institution_name": "Institution '"$NAME"'"
+        }'
 }
 
 echo How much courses do you want to insert?
@@ -32,5 +32,5 @@ read courses_quantity
 
 for i in `seq 1 $courses_quantity`;
 do
-	postCourse
+    postCourse
 done
--- populate-institutions.sh
+++ populate-institutions.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
+
+FLOOR=111111111111
+CEILING=999999999999
+
 function postInstitution {
-	FLOOR=111111111111
-	CEILING=999999999999
-	NAME=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
-	curl -X POST \
-	  https://athena-bff.herokuapp.com/v1/institutions \
-	  -H 'cache-control: no-cache' \
-	  -H 'content-type: application/json' \
-	  -H 'postman-token: a0ff56d8-4393-d1a3-7960-fd27fc61edae' \
-	  -d '{
-		"name": "Institution '"$NAME"'",
-		"image_url": "https://www.insitution'"$NAME"'.com.br/images/logo.png",
-	    	"website": "https://www.'"$NAME"'.com.br/",
-	    	"cnpj": "59.069.914/0001-51",
-	    	"address": "Rua '"$NAME"', '"$RANDOM"' - Barra Funda",
-	    	"city": "São Paulo",
-	    	"province": "SP",
-		"emails": [
-			"institution'"$NAME"'@gmail.com",
-			"institution'"$NAME"'@institution.edu.br"
-		],
-		"phones": [
-			"+'"$(shuf -i $FLOOR-$CEILING -n 1)"'",
-			"+'"$(shuf -i $FLOOR-$CEILING -n 1)"'"
-		]
-	   }'
+    NAME=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
+
+    curl -X POST \
+    https://athena-bff.herokuapp.com/v1/institutions \
+        -H 'cache-control: no-cache' \
+        -H 'content-type: application/json' \
+        -H 'postman-token: a0ff56d8-4393-d1a3-7960-fd27fc61edae' \
+        -d '{
+            "name": "Institution '"$NAME"'",
+            "image_url": "https://www.insitution'"$NAME"'.com.br/images/logo.png",
+            "website": "https://www.'"$NAME"'.com.br/",
+            "cnpj": "59.069.914/0001-51",
+            "address": "Rua '"$NAME"', '"$RANDOM"' - Barra Funda",
+            "city": "São Paulo",
+            "province": "SP",
+            "emails": [
+                "institution'"$NAME"'@gmail.com",
+                "institution'"$NAME"'@institution.edu.br"
+            ],
+            "phones": [
+                "+'"$(shuf -i $FLOOR-$CEILING -n 1)"'",
+                "+'"$(shuf -i $FLOOR-$CEILING -n 1)"'"
+            ]
+        }'
 }
 
 echo How much institutions do you want to insert?
@@ -33,7 +36,5 @@ read institutions_quantity
 
 for i in `seq 1 $institutions_quantity`;
 do
-	postInstitution
+    postInstitution
 done
-
-set -x
